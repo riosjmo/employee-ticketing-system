e0bfa728bd909fc89dce41d498e7f96634b2cae0
rio <riojordan127@yahoo.com>
2025-11-28 17:18:49 -0600

    chore: implement and test tickets CRUD endpoints

--- server/src/config/prisma.ts
+++ server/src/config/prisma.ts
@@ -1,10 +1,5 @@
-import pkg from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
-// Support different Prisma client export shapes across versions:
-// - older: `import { PrismaClient } from '@prisma/client'`
-// - newer: default or namespace with `PrismaClient` property
-const PrismaClientCtor = (pkg as any).PrismaClient ?? (pkg as any).default ?? pkg
+const prisma = new PrismaClient();
 
-const prisma = new (PrismaClientCtor as any)()
-
-export default prisma
+export default prisma;
