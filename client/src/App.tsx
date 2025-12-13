ea22700cda48eebdddc58524719eebaf526fadb5
rio <riojordan127@yahoo.com>
2025-12-13 13:09:51 -0600

    Render tickets page with loading and error states

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,6 @@
-import { useEffect } from "react";
-import { getTickets } from "./api/tickets.api";
+import { RouterProvider } from "react-router-dom"
+import router from "./router"
 
-function App() {
-  useEffect(() => {
-    getTickets()
-      .then((tickets) => {
-        console.log("Tickets from API:", tickets);
-      })
-      .catch(console.error);
-  }, []);
-
-  return (
-    <div>
-      <h1>Ticket System</h1>
-      <p>Check the console for API response</p>
-    </div>
-  );
+export default function App() {
+  return <RouterProvider router={router} />
 }
-
-export default App;
